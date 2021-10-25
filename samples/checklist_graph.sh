1bdca08ef187ec9fe67c04c1323d234e5f0d2f4a
Jose Moreno <jose.moreno@microsoft.com>
2021-10-25 16:47:37 +0200

    added guids

--- samples/checklist_graph.sh
+++ samples/checklist_graph.sh
@@ -10,10 +10,11 @@
 #    -b/--base-url: URL where to download the JSON file from. Defaults to https://raw.githubusercontent.com/Azure/review-checklists/main/samples/
 #    -t/--technology: technology to verify. Accepted values lz, aks and avd. Defaults to aks
 #    -c/--category: category to filter the tests. Accepted values: 0 to (number_of_categories-1)
-#    -d/--debug: increase verbosity
 #    -l/--list-categories: instead of running Azure Graph queries, it only displays the categories in the file and its corresponding indexes
 #    -s/--show-text: shows the check text (good for troubleshooting, not so good to copy/paste to Excel)
 #    -n/--no-empty: does not show checks without an associated query
+#    -o/--output: can be either console or json
+#    -d/--debug: increase verbosity
 #
 # Example:
 #       ./checklist_graph.sh -l -t=aks
@@ -33,6 +34,7 @@ error_file=/tmp/error.json
 list_categories=no
 check_text=no
 no_empty=no
+output=console
 
 # Color format variables
 normal="\e[0m"
@@ -70,6 +72,10 @@ do
           -n*|--no-empty*)
                no_empty="yes"
                shift # past argument with no value
+               ;;
+          -o=*|--output=*)
+               output="${i#*=}"
+               shift # past argument=value
                ;;
           -d*|--debug*)
                debug="yes"
@@ -129,12 +135,14 @@ then
     graph_failure_list=$(echo $checklist_json | jq -r '.items[] | select(.category=="'$category_name'") | .graph_failure')
     category_list=$(echo $checklist_json | jq -r '.items[] | select(.category=="'$category_name'") | .category')
     text_list=$(echo $checklist_json | jq -r '.items[] | select(.category=="'$category_name'") | .text')
+    guid_list=$(echo $checklist_json | jq -r '.items[] | select(.category=="'$category_name'") | .guid')
     if [[ "$debug" == "yes" ]]; then echo "DEBUG: $(echo $category_list | wc -l) tests found in the checklist for category ${category_name}."; fi
 else
     graph_success_list=$(echo $checklist_json | jq -r '.items[] | .graph_success')
     graph_failure_list=$(echo $checklist_json | jq -r '.items[] | .graph_failure')
     category_list=$(echo $checklist_json | jq -r '.items[] | .category')
     text_list=$(echo $checklist_json | jq -r '.items[] | .text')
+    guid_list=$(echo $checklist_json | jq -r '.items[] | .guid')
     if [[ "$debug" == "yes" ]]; then echo "DEBUG: $(echo $category_list | wc -l) tests found in the checklist."; fi
 fi
 
@@ -147,26 +155,33 @@ extension_name=resource-graph
 extension_version=$(az extension show -n $extension_name --query version -o tsv 2>/dev/null)
 if [[ -z "$extension_version" ]]
 then
-    echo "Azure CLI extension $extension_name not found, installing now..."
+    if [[ "$debug" == "yes" ]]; then echo "DEBUG: Azure CLI extension $extension_name not found, installing now..."; fi
     az extension add -n $extension_name -o none 2>/dev/null
 else
-    echo "Azure CLI extension $extension_name found with version $extension_version, trying to upgrade..."
+    if [[ "$debug" == "yes" ]]; then echo "DEBUG: Azure CLI extension $extension_name found with version $extension_version, trying to upgrade..."; fi
     az extension update -n $extension_name -o none 2>/dev/null
 fi
 extension_version=$(az extension show -n $extension_name --query version -o tsv 2>/dev/null)
-echo "Azure CLI extension $extension_name running with version $extension_version"
+if [[ "$debug" == "yes" ]]; then echo "Azure CLI extension $extension_name running with version $extension_version"; fi
 
 # Run queries
 i=0
 this_category_name=""
+json_output="{ \"checks\": ["
 while IFS= read -r graph_success_query; do
     i=$(($i+1))
-    #if [[ "$debug" == "yes" ]]; then echo "Processing check item $i..."; fi
+    this_guid=$(echo $guid_list | head -$i | tail -1)
+    if [[ "$debug" == "yes" ]]; then echo "Processing check item $i, GUID $this_guid..."; fi
+    if [[ "$this_guid" == "null" ]] && [[ "$output" == "json" ]]; then
+        if [[ "$debug" == "yes" ]]; then echo "ERROR: GUID not defined for check number $i"; fi
+    fi
     # Print header category if required
     if [[ "$this_category_name" != "$(echo $category_list | head -$i | tail -1)" ]]
     then
         this_category_name=$(echo $category_list | head -$i | tail -1)
-        echo "${bold}${blue}CHECKLIST CATEGORY: ${this_category_name}${normal}"
+        if [[ "$output" == "console" ]]; then
+            echo "${bold}${blue}CHECKLIST CATEGORY: ${this_category_name}${normal}"
+        fi
     fi
     # Check if there is any query
     if [[ "$graph_success_query" == "null" ]]; then
@@ -181,7 +196,7 @@ while IFS= read -r graph_success_query; do
         fi
     else
         # Print title if required
-        if [[ "$check_text" == "yes" ]]; then
+        if [[ "$check_text" == "yes" ]] && [[ "$output" == "console" ]]; then
             this_text=$(echo $text_list | head -$i | tail -1)
             echo "CHECKLIST ITEM: ${this_text}:"
         fi
@@ -207,16 +222,33 @@ while IFS= read -r graph_success_query; do
         if [[ -z "$success_result" ]]; then success_result='None'; fi
         if [[ -z "$failure_result" ]]; then failure_result='None'; fi
         # Print output in color format
-        if [[ "$success_result" == "None" ]]; then
-            success_color=$yellow
-        else
-            success_color=$green;
+        if [[ "$output" == console ]]; then
+            if [[ "$success_result" == "None" ]]; then
+                success_color=$yellow
+            else
+                success_color=$green;
+            fi
+            if [[ "$failure_result" == "None" ]]; then
+                failure_color=$green
+            else
+                failure_color=$red;
+            fi
+            echo "Success: ${success_color}${success_result}${normal}. Fail: ${failure_color}${failure_result}${normal}"
         fi
-        if [[ "$failure_result" == "None" ]]; then
-            failure_color=$green
-        else
-            failure_color=$red;
+        # Append JSON info
+        # First, add a comma if this wasnt the first element
+        if [[ "$json_output" != "{ \"checks\": [" ]]; then
+            json_output+=", "
         fi
-        echo "Success: ${success_color}${success_result}${normal}. Fail: ${failure_color}${failure_result}${normal}"
+        # Now add the JSON element
+        json_output+="{\"guid\": \"$this_guid\", \"success\": \"$success_result\", \"failure\": \"$failure_result\"}"
     fi
 done <<< "$graph_success_list"
+
+# Close JSON format
+json_output+="]}"
+
+# If output is JSON, print check_results variable
+if [[ "$output" == "json" ]]; then
+    echo "$json_output"
+fi
