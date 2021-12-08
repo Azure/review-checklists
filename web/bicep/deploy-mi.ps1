ae024aa1ec979b72f442fd7503aac4730b33f8c1
Fletcher Kelly <flkelly@microsoft.com>
2021-12-08 07:45:47 +0000

    some error checking added

--- web/bicep/deploy-mi.ps1
+++ web/bicep/deploy-mi.ps1
@@ -1,8 +1,24 @@
+## Update Variables a you need to
 $rgName = "flkelly-checklist"
 $rgLocation = "westeurope"
 
-New-AzResourceGroup -Name $rgName -Location $rgLocation
+## Test to see if Resource Group exists
+Write-Host "Checking if $rgname Resource Group Exists" -nonewline 
+$test = Get-AzResourceGroup -Name $rgName -Location $rgLocation -ErrorAction SilentlyContinue
+if ($null -eq $test) {
+    Write-Host -ForegroundColor red " [NO]"
+    Write-Host "$rgname Resource Group not found - creating it"
+    New-AzResourceGroup -Name $rgName -Location $rgLocation
+} else {
+    Write-Host -ForegroundColor green " [YES]"
+}
 
 $deploymentFile = ".\main.bicep"
-$deploymentName = (($deploymentFile).Substring(2)).Replace("\","-") + "-" +(get-date -Format ddMMyyyy-hhmmss) + "-deployment"
-New-AzResourceGroupDeployment -TemplateFile $deploymentFile -Name $deploymentName -ResourceGroupName $rgName
+
+try {
+    $deploymentName = (($deploymentFile).Substring(2)).Replace("\","-") + "-" +(get-date -Format ddMMyyyy-hhmmss) + "-deployment"
+    New-AzResourceGroupDeployment -TemplateFile $deploymentFile -Name $deploymentName -ResourceGroupName $rgName
+}
+catch {
+    write-host "Failure - $_"
+}
